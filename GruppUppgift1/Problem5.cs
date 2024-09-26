643de8a44604bf9116cfa4462d3ac26a850bb5ac
Martin Kåår Johansson <martin@kaarjohansson.se>
2024-09-26 21:54:11 +0200

    Update Problem5.cs

--- GruppUppgift1/Problem5.cs
+++ GruppUppgift1/Problem5.cs
@@ -2,6 +2,44 @@ class Problem5
 {
     public static void Run()
     {
-        Console.WriteLine("Problem");
+
+        string ChangePlaceDuJag(string input)
+        {
+            // Då inbygga funktioner som .Replace returnerar en 
+            // helt ny sträng kan vi inte köra som exemepel: string.Replace("Du", "Jag").Replace("Jag", "Du")
+            // Så vi får köra en hederlig for loop. 
+            string returnString = "";
+            foreach (string word in input.Split(null))
+            {
+                if (word.Equals("Du"))
+                {
+                    returnString += "Jag ";
+                }
+                else if (word.Equals("du"))
+                {
+                    returnString += "jag ";
+                }
+                else if (word.Equals("Jag"))
+                {
+                    returnString += "Du ";
+                }
+                else if (word.Equals("jag"))
+                {
+                    returnString += "du ";
+                }
+                else
+                {
+                    returnString += $"{word} ";
+                }
+            }
+
+            return returnString.Trim();
+        }
+
+        Console.Write("Ange din sträng: ");
+        string output = ChangePlaceDuJag(Console.ReadLine()!);
+
+        Console.WriteLine(output);
     }
+
 }

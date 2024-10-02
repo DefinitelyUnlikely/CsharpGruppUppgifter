796397f5f159d2618564c02535e2a3ecf7af7e21
DefinitelyUnlikely <Martin@kaarjohansson.se>
2024-10-02 17:10:58 +0200

    Gör lite ändringar i problem 1. Tar bort hårdkodningen av vilken array som skickas in och ber istället användaren om input

--- GruppUppgift1/Problem1.cs
+++ GruppUppgift1/Problem1.cs
@@ -1,13 +1,31 @@
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Net;
+
 public class Problem1
 {
     public static void Run()
     {
-        int[] MinAndMax(int[] ints)
+
+        // Om man vill hade denna funktion kunna innehålla allt som är inuti run
+        // och vi skippar returnen. Men jag tänker att en funktion som bara skickar tillbaka 
+        // min och max för en int[] är mer återandvändbar än en som tvingar dig att skriva in saker i 
+        // terminalen etc. 
+        static int[] MinAndMax(int[] ints)
         {
             return [ints.Min(), ints.Max()];
         }
 
-        int[] minMax = MinAndMax([1, 2, 3, 4, 5]);
+        Console.Write("Please enter numbers separated by whitespace: ");
+        string[] strNumbers = Console.ReadLine()!.Split(null);
+
+        // Då vi inte behöver/skall felhantera eller validera, kör vi Parse istället för TryParse.
+        // Då krashar programmet så fort användaren gör fel.
+        int counter = 0;
+        int[] ints = new int[strNumbers.Length];
+        Array.ForEach(strNumbers, strNum => { ints[counter++] = int.Parse(strNum); });
+
+        int[] minMax = MinAndMax(ints);
         Console.WriteLine($"Min value: {minMax[0]} Max Value: {minMax[1]}.");
     }
 }

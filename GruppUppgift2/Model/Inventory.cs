9bcdb2fa91d374769809be7b304ccc463c5d3ab1
Liam-EV <L-edmalm@hotmail.com>
2024-10-10 10:40:19 +0200

    Create Inventory.cs

---
+++ GruppUppgift2/Model/Inventory.cs
@@ -0,0 +1,49 @@
+public class Inventory
+{
+    private List<UsableItem> items;
+
+    public Inventory()
+    {
+        items = new List<UsableItem>();
+    }
+
+    public void AddItem(UsableItem item)
+    {
+        items.Add(item);
+        Console.WriteLine($"{item.} has been added to your inventory.");
+    }
+
+    public void RemoveItem(UsableItem item)
+    {
+        if (items.Contains(item))
+        {
+            items.Remove(item);
+            Console.WriteLine($"{item} has been removed from your inventory.");
+        }
+        else
+        {
+            Console.WriteLine($"{item} is not in your inventory.");
+        }
+    }
+
+    public bool CheckInventory(UsableItem item)
+    {
+        return items.Contains(item);
+    }
+
+    public void ListInventory()
+    {
+        if (items.Count == 0)
+        {
+            Console.WriteLine("Your inventory is empty.");
+        }
+        else
+        {
+            Console.WriteLine("Your inventory contains:");
+            foreach (var item in UsableItem)
+            {
+                Console.WriteLine($"- {item}");
+            }
+        }
+    }
+}

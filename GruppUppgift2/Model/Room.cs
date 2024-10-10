3de8470b6055f5d79005c21c0448bfdb37bd40c4
Martin Kåår Johansson <martin@kaarjohansson.se>
2024-10-10 12:45:03 +0200

    Update Room.cs
    Uppdaterar med de tankar ajg hade + mergar med det Olof lagt till.

--- GruppUppgift2/Model/Room.cs
+++ GruppUppgift2/Model/Room.cs
@@ -1,17 +1,40 @@
+namespace GruppUppgift2.Model;
+
 public class Room : GameObject
 {
-    public List<GameObject> Items { get; set; }
+
+    public string[] ConnectsToRoom { get; }
+    public List<GameObject> Items { get; }
+    // Tillagt av Olof, så det är kopplat till story.
     public Story story;
 
-    public Room(string name, string description, List<GameObject> items, Story story)
-    //: base(name, description)
+    public Room(string name, string description, string[] connectsToRoom, Story story, List<GameObject>? items = null)
+    : base(name, description)
     {
-        Items = items;
+        ConnectsToRoom = connectsToRoom;
+        Items = items ?? [];
         this.story = story;
     }
 
+    // Vi vill kunna uppdatera vårt rums description om något i rummet ändras. 
+    // En metod som antagligen ska kallas på av StoryManager eller GameManager.
+    // Eller så sköts det helt av story, med kapitel eller dylikt. Vi får se.
+    public void UpdateDescription(string newDescription)
+    {
+        Description = newDescription;
+    }
+
+    // Vet inte hur man tänker sig interagera med Room objekt, så jag lägger till denna tills vidare.
+    public virtual void StartRoom()
+    {
+        Console.WriteLine("This is just the base class for the Room Object, ");
+    }
+
+    // Tillagt av Olof, så det är kopplat till story.
+    // Tyder på att vi inte kommer behöva StartRoom. 
     public void PrintStory()
     {
         Console.WriteLine(story.GetStoryDescription());
     }
+
 }

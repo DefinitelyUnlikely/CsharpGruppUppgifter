22652c8a03c51a2b4cfc9458592531075e3459d1
Martin Kåår Johansson <martin@kaarjohansson.se>
2024-10-10 12:35:02 +0200

    Update Player.cs

--- GruppUppgift2/Model/Player.cs
+++ GruppUppgift2/Model/Player.cs
@@ -1,7 +1,12 @@
+namespace GruppUppgift2.Model;
+
 public class Player
 {
     string Name { get; set; }
     bool[] completedRooms = { false, false, false, false, false };
+    public Inventory inventory = new Inventory();
 
-    int currentRoom = 0;
+    // Om vi tänkte identifiera rum med namn, tänker jag att vi kör str istället för int.
+    string currentRoom = "";
+
 }

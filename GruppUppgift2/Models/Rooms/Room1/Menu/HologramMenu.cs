ad17d5b11661c81557435be842ded0ecd7766c50
CodeDevJA <codedev.ja@gmail.com>
2024-11-03 03:26:57 +0100

    Update and rename BeachMenu.cs to HologramMenu.cs
    Update and rename BeachMenu.cs to HologramMenu.cs + AddCommand to list.

--- GruppUppgift2/Models/Rooms/Room1/Menu/HologramMenu.cs
+++ GruppUppgift2/Models/Rooms/Room1/Menu/HologramMenu.cs
@@ -1,9 +1,10 @@
-public class BeachMenu : Menu
+public class HologramMenu : Menu
 {
-    public BeachMenu()
+    public HologramMenu()
     {
         AddCommand(new UseCommand());
         AddCommand(new TakeCommand());
+        AddCommand(new InventoryCommand());
         AddCommand(new InspectCommand());
         AddCommand(new StoryCommand());
         AddCommand(new LeaveCommand());
